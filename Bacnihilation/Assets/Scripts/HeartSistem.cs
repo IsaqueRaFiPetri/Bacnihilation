00d9b88cba843c667c3ca034a12a2ae44020b3ee
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-03-26 14:25:26 -0300

    Vida
    Só falta fazer o GameOver e o Dano

---
+++ Bacnihilation/Assets/Scripts/HeartSistem.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class HeartSistem : MonoBehaviour
+{
+    public int life;
+    public int maxLife;
+
+    public Image[] heart;
+    public Sprite full;
+    public Sprite empty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthLogic();
+    }
+    void HealthLogic()
+    {
+        if (life > maxLife)
+        {
+            life = maxLife;
+        }
+        for (int i = 0; i < heart.Length; i++)
+        {
+            if(i < life)
+            {
+                heart[i].sprite = full;
+            }
+            else
+            {
+                heart[i].sprite = empty;
+            }
+
+            if (i < maxLife)
+            {
+                heart[i].enabled = true;
+            }
+            else
+            {
+                heart[i].enabled = false;
+            }
+        }
+    }
+}
+//https://www.youtube.com/watch?v=6SAbQfz3QRI
+//
